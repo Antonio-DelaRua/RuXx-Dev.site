0414ff839b85377c7f596e47026ac9141416e7bc
Antonio-DelaRua <ruxxdeveloper@gmail.com>
2022-10-04 23:28:07 +0200

    04/10/22

--- src/app/pages/concesionario/concesionario.component.ts
+++ src/app/pages/concesionario/concesionario.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PostService} from 'src/app/post.service'
+import { Coches } from 'src/app/post.model';
 
 @Component({
   selector: 'app-concesionario',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./concesionario.component.css']
 })
 export class ConcesionarioComponent implements OnInit {
-
-  constructor() { }
+  Coches: Coches[] ;
+  constructor(private postService: PostService) { }
 
   ngOnInit(): void {
+    this.postService.getPosts().subscribe( (res) => {
+      this.Coches = res.map ( (e) =>{
+        return {
+          id: e.payload.doc.id,
+          ...(e.payload.doc.data() as Coches)
+        };
+      });
+    });
+
+  }
   }
 
-}
+
