cdaaf2a4bdad22b7477d28c99e3e5374511262da
Antonio-DelaRua <ruxxdeveloper@gmail.com>
2023-06-14 22:45:53 +0200

    final-V.0.0.1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-
 import { CreateCarComponent } from './pages/create-car/create-car.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ConcesionarioComponent } from './pages/concesionario/concesionario.component';
